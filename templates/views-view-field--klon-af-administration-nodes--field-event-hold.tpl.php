edbb26920930ce4f3e966fb4b6d31442c74813b1
memleakz <dev@dev-VirtualBox>
2019-02-07 15:15:48 +0100

    integrate obb_events_teams in views and logic

---
+++ templates/views-view-field--klon-af-administration-nodes--field-event-hold.tpl.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($row->field_field_event_hold))
+{
+    foreach($row->field_field_event_hold as $i => $item)
+    {
+        $entity = entity_load_single('field_collection_item',$item['raw']['value']);
+        $bg_color =  $entity->field_efterbehandling_workflow['und'][0]['value'] != 30 ? 'green' : 'red'; // get workflow id from name..
+        if($i == 0)
+        {
+            echo "<a target='_blank' href='/field-collection/field-event-hold/".$entity->item_id."/edit'><div style='display:inline-block;color:".$bg_color."';'>".$entity->field_hold_navn['und'][0]['value']."</div></a>";
+        }
+        else
+        {
+             echo ", <a target='_blank' href='/field-collection/field-event-hold/".$entity->item_id."/edit'><div style='display:inline-block;color:".$bg_color.";'>".$entity->field_hold_navn['und'][0]['value']."</div></a>";
+        }
+    }
+}
+//echo "test";
