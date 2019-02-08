9f9053fb7d083fd342a214247738797f5e9faa51
memleakz <dev@dev-VirtualBox>
2019-02-08 14:18:24 +0100

    more integration

--- templates/views-view-field--klon-af-administration-nodes--field-event-hold.tpl.php
+++ templates/views-view-field--klon-af-administration-nodes--field-event-hold.tpl.php
@@ -7,11 +7,11 @@ if(isset($row->field_field_event_hold))
         $bg_color =  $entity->field_efterbehandling_workflow['und'][0]['value'] != 30 ? 'green' : 'red'; // get workflow id from name..
         if($i == 0)
         {
-            echo "<a target='_blank' href='/field-collection/field-event-hold/".$entity->item_id."/edit'><div style='display:inline-block;color:".$bg_color."';'>".$entity->field_hold_navn['und'][0]['value']."</div></a>";
+            echo "redigér <a href='/field-collection/field-event-hold/".$entity->item_id."/edit'><div style='display:inline-block;color:".$bg_color."';'>".$entity->field_hold_navn['und'][0]['value']."</div></a>";
         }
         else
         {
-             echo ", <a target='_blank' href='/field-collection/field-event-hold/".$entity->item_id."/edit'><div style='display:inline-block;color:".$bg_color.";'>".$entity->field_hold_navn['und'][0]['value']."</div></a>";
+             echo ", <a href='/field-collection/field-event-hold/".$entity->item_id."/edit'><div style='display:inline-block;color:".$bg_color.";'>".$entity->field_hold_navn['und'][0]['value']."</div></a>";
         }
     }
 }
